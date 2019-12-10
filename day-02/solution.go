0719b7297ab346f6d64708d0e2620302c0294977
Jonathan Moody <moodyofjonathan@gmail.com>
2019-12-09 23:17:59 -0800

    Initial commit

---
+++ day-02/solution.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"io/ioutil"
+  "strconv"
+  "strings"
+)
+
+func main() {
+	input, _ := ioutil.ReadFile("./input.txt")
+  var opcodes []string = strings.Split(string(input), ",")
+  opcodes[1] = "12"
+  opcodes[2] = "2"
+
+  type Operation int
+
+  const (
+    Add Operation = iota
+    Multiply
+  )
+
+  var op Operation
+  var a int
+  var b int
+
+  Loop: for i, code := range opcodes {
+    var c, _ = strconv.Atoi(code)
+
+    switch i % 4 {
+    case 0:
+      switch code {
+      case "99":
+        break Loop
+      case "1":
+        op = Add
+      case "2":
+        op = Multiply
+      default:
+        println("ERROR")
+    }
+    case 1:
+      a, _ = strconv.Atoi(opcodes[c])
+    case 2:
+      b, _ = strconv.Atoi(opcodes[c])
+    case 3:
+      switch op {
+        case Add:
+          opcodes[c] = strconv.Itoa(a + b)
+        case Multiply:
+          opcodes[c] = strconv.Itoa(a * b)
+      }
+    }
+  }
+
+  println(opcodes[0])
+}
