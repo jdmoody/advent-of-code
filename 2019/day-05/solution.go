ac6237bca67cfb712196c64199731811459d6758
Jonathan Moody <moodyofjonathan@gmail.com>
2019-12-26 11:40:17 -0800

    Add back day 4, move day 5 to correct directory

---
+++ 2019/day-05/solution.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/jdmoody/advent-of-code/2019/intcode"
+)
+
+func main() {
+	opcodes := intcode.GetOpcodes("day-05")
+
+	var computer intcode.Computer = intcode.New(opcodes)
+
+	computer.Run()
+}
