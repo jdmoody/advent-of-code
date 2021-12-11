0b9ec4400fbe5c2ae039cb094a2bdd3067278306
Jonathan Moody <jonathan@medium.com>
2021-12-11 15:10:01 -0800

    Clean up 2021/1 solution

--- 2021/1/solution.cr
+++ 2021/1/solution.cr
@@ -1,37 +1,35 @@
+def calculate_depth_increases(depths : Array(String), window_size = 1)
+  return depths.each
+    .map { |depth| depth.to_i }
+    .reduce({prev: nil, window: [] of Int32, increases: 0}) do |state, depth|
+
+      # Update the window
+      state[:window].push depth
+      if state[:window].size > window_size
+        state[:window].shift
+      end
+
+      # If have a full window, start doing depth comparisons
+      if state[:window].size === window_size
+        avg_depth = state[:window].reduce { |acc, d| acc + d }
+
+        # If the depth has increased, increment
+        if state[:prev].try &.< avg_depth
+          next {prev: avg_depth, window: state[:window], increases: state[:increases] + 1}
+        end
+        
+        # Otherwise, do nothing
+        next {prev: avg_depth, window: state[:window], increases: state[:increases]}
+      end
+
+      next {prev: nil, window: state[:window], increases: state[:increases]}
+    end[:increases]
+end
+
 depths = File.read_lines("./input.txt")
+
 # Part 1
-depth_state = depths.each
-  .map { |depth| depth.to_i }
-  .reduce({prev: nil, increases: 0}) do |state, depth|
-    if state[:prev].try &.< depth
-      next {prev: depth, increases: state[:increases] + 1}
-    end
-
-    next {prev: depth, increases: state[:increases]}
-  end
-
-puts depth_state[:increases]
+puts calculate_depth_increases depths
 
 # Part 2
-depth_state = depths.each
-  .map { |depth| depth.to_i }
-  .reduce({prev: nil, window: [] of Int32, increases: 0}) do |state, depth|
-    # Update the window
-    state[:window].push depth
-    if state[:window].size > 3
-      state[:window].shift
-    end
-		
-		# If the new window had a depth increase, update the state
-		if state[:window].size === 3
-      avg_depth = state[:window].reduce { |acc, d| acc + d }
-      if state[:prev].try &.< avg_depth
-        next {prev: avg_depth, window: state[:window], increases: state[:increases] + 1}
-      end
-        next {prev: avg_depth, window: state[:window], increases: state[:increases]}
-		end
-
-    next {prev: nil, window: state[:window], increases: state[:increases]}
-  end
-
-puts depth_state[:increases]
+puts calculate_depth_increases depths, window_size: 3
